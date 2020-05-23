7a9a97bbe4c0f11bf5747516560b156f7bf324c8
Adedipupo <bayonleolaluwa@gmail.com>
2020-05-23 02:54:00 +0100

    wip

--- src/Product.js
+++ src/Product.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Product = (props) => {
 return(
   <div>
-     <h2>{props.products.name}</h2>
+     <h2>{props.product.name}</h2>
     <p>{props.product.price.toLocaleString("en-US",
      { style: "currency", currency: "USD" })} - {props.product.description}</p>
   </div>
