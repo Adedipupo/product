fceb8e0346bcba63c1f757fee3e28fe9eb540a1d
Adedipupo <bayonleolaluwa@gmail.com>
2020-05-23 02:24:33 +0100

    wip

--- src/vschoolProjects.js
+++ src/vschoolProjects.js
@@ -5,3 +5,9 @@ const products = [
         price: 1,
         description: "Perfect for those who can't remember things! 5/5 Highly recommend."
     },
+    {
+        id: "2",
+        name: "Housing",
+        price: 0,
+        description: "Housing provided for out-of-state students or those who can't commute"
+    },
