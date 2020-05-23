eab3a0d1e691f615b0d4d3b5e1beda7ccdcf6796
Adedipupo <bayonleolaluwa@gmail.com>
2020-05-23 02:42:30 +0100

    wip

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import productsData from './productsData';
 import './App.css';
 import products from './productsData';
+import Product from '../Product';
 
 function App() {
 
   return (
     <div>
-hello weee
+      <Product />
     </div>
   );
 }
